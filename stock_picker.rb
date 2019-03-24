03613bd0d7c93016ad7c8cfc6748a99f40198120
vollantre <rinbinary@gmail.com>
2019-03-24 09:55:12 -0400

    yay

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,17 +1,9 @@
 def stock_picker stocks
-  pair = []
-  
-  if stocks.size < 2
-    return "Please enter an array with a valid number of stocks"
-  else
-    stocks.
-      each_with_index.
-        to_a.
-          combination(2).
-            reduce(0) do |(val, acc), ((v1, idx1), (v2, idx2))|
-            val < v2 - v1 ? [v2 - v1, [idx1, idx2]] : [val, acc]
-    end
-  end
+  res = stocks.each_with_index.with_object([]) do |(buy_val, i), res|
+    highest_val = stocks[i..].max
+    highest_idx = stocks[i..].each_with_index.max[1] + i
+    res << [highest_val - buy_val, i, highest_idx]
+  end.max_by(&:first)
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
